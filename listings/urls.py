13a3c09d7e7876d0370c41de7fe71e8c2712f082
AYOUB <ayoub.ardem@gmail.com>
2025-12-14 15:17:39 +0100

    Add payment initiation and verification URLs

--- listings/urls.py
+++ listings/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path  
 from rest_framework.routers import DefaultRouter
 from .views import ListingViewSet, BookingViewSet
+from .views import initiate_payment, verify_payment
 
-urlpatterns=[
+urlpatterns = [
+    path("payment/initiate/", initiate_payment),
+    path("payment/verify/", verify_payment),
 ]
+
 
 router = DefaultRouter()
 router.register(r'listings', ListingViewSet, basename='listings')
